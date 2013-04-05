cc2155379bbf744046a0fe0e5ea532f5c2e95109
Laura Macaddino <l.macaddino@gmail.com>
2013-04-05 01:57:17 -0500

    Personal Version of Project1a

---
+++ tests/runners.py
@@ -0,0 +1,132 @@
+import HTMLTestRunner
+import unittest
+import tests
+import sys
+
+def find_test(obj, name):
+    if isinstance(obj, unittest.suite.TestSuite):
+        for t in obj:
+            r = find_test(t, name)
+            if r is not None:
+                return r
+    else:
+        if isinstance(obj, tests.common.ChircTestCase) and obj._testMethodName == name:
+            return obj
+        else:
+            return None
+
+def get_test_list(obj):
+    l = []
+    if isinstance(obj, unittest.suite.TestSuite):
+        for t in obj:
+            l += get_test_list(t)
+        return l
+    else:
+        if isinstance(obj, tests.common.ChircTestCase):
+            return [obj]
+        else:
+            return []
+
+def get_scores(result):
+    alltests = get_test_list(tests.alltests)
+    alltests_methods = dict([(t._testMethodName, getattr(t, t._testMethodName)) for t in alltests])
+
+    failed_names = set([t._testMethodName for t,e in result.errors + result.failures])
+
+    scores = {}
+    for proj in tests.scores.PROJECTS:
+        scores[proj.name] = {}
+        for cid in proj.lcategories:
+	    ctests_names = set([name for name, method in alltests_methods.items() if method.category == cid and method.points == True])
+            num_total = len(ctests_names)
+            num_failed = len(ctests_names & failed_names)
+            num_success = num_total - num_failed
+            scores[proj.name][cid] = (num_success, num_failed, num_total)
+    return scores
+
+def html_runner(html_file):
+    # output to a file
+    fp = file(html_file, 'wb')
+    runner = HTMLTestRunner.HTMLTestRunner(
+                stream=fp,
+                verbosity = 0,
+                title='chirc',
+                description='Results of running all chirc unit tests'
+                )
+    
+    # run the test
+    result = runner.run(tests.alltests)
+    return result
+
+def grade_runner(csv, htmlfile = None, exe = None, fast = 0):
+    if exe != None:
+        tests.common.ChircTestCase.CHIRC_EXE = exe
+
+    if fast == 1:
+        tests.common.ChircTestCase.MESSAGE_TIMEOUT = 0.2
+        tests.common.ChircTestCase.INTERTEST_PAUSE = 0.0
+    else:
+        tests.common.ChircTestCase.MESSAGE_TIMEOUT = 1.0
+        tests.common.ChircTestCase.INTERTEST_PAUSE = 0.15
+        
+    if htmlfile is not None:
+        fp = file(htmlfile, 'wb')
+        runner = HTMLTestRunner.HTMLTestRunner(
+                    stream=fp,
+                    verbosity = 0,
+                    title='chirc',
+                    description='Results of running all chirc unit tests'
+                    )
+    else:
+        if not csv:
+            stream = sys.stderr
+        else:
+            stream = open("/dev/null", "w")
+
+        runner = unittest.TextTestRunner(stream = stream)
+    
+    # run the test
+    result = runner.run(tests.alltests)
+
+    scores = get_scores(result)
+
+    pscores = []
+    for proj in tests.scores.PROJECTS:
+        pscore = 0.0
+        if not csv:
+            print proj.name
+            print "=" * 73
+            print "%-35s %-6s / %-10s  %-6s / %-10s" % ("Category", "Passed", "Total", "Score", "Points")
+            print "-" * 73
+        for cid in proj.lcategories:
+            (num_success, num_failed, num_total) = scores[proj.name][cid]
+            cname, cpoints = proj.categories[cid]
+
+            if num_total == 0:
+                cscore = 0.0
+            else:
+                cscore = (float(num_success) / num_total) * cpoints
+
+            pscore += cscore
+            
+            if not csv:
+                print "%-35s %-6i / %-10i  %-6.2f / %-10.2f" % (cname, num_success, num_total, cscore, cpoints)
+        if not csv:
+            print "-" * 73
+            print "%54s = %-6.2f / %-10i" % ("TOTAL", pscore, proj.points)
+            print "=" * 73
+            print
+        pscores.append(pscore)
+    if csv:
+        print ",".join([`s` for s in pscores])
+    return result
+
+def single_runner(test_name):
+    t = find_test(tests.alltests, test_name)
+    
+    if t is None:
+        return
+    
+    tests.DEBUG = True
+    runner = unittest.TextTestRunner()   
+    runner.run(t)
---
+++ tests/src/main.c
@@ -0,0 +1,125 @@
+/*
+ *
+ *  CMSC 23300 / 33300 - Networks and Distributed Systems
+ *
+ *  main() code for chirc project
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+/* parser: 
+ * Given an array containing a command, its number of bytes,
+ * and an empty 2D char array, fills the 2D char array with
+ * each individual word in the command and returns the 
+ * number of arguments in the command (total number of words
+ * in the command minus one).
+ */
+int parser(char *input,  int nbytes, char **argList)
+{
+	//terminates with "\r\n"
+
+	int maxArgs = 15;	
+	int array_index = 0;
+	int start_cpy = 0;
+	int n;
+	for (n=0; n<nbytes-1; n++)
+	{
+		if (input[n] == ' ')
+		{
+			int argSize = n-1-start_cpy;
+			argList[array_index] = (char *) malloc(argSize);
+			memcpy(argList[array_index], input+start_cpy, argSize);
+			start_cpy = n+1;
+			array_index++;
+		}
+		if (array_index == maxArgs)
+		{
+			break;
+		}
+	}
+	return array_index;
+}
+
+int main(int argc, char *argv[])
+{
+	int opt;
+	char *port = "6667", *passwd = NULL;
+
+	while ((opt = getopt(argc, argv, "p:o:h")) != -1)
+		switch (opt)
+		{
+			case 'p':
+				port = strdup(optarg);
+				break;
+			case 'o':
+				passwd = strdup(optarg);
+				break;
+			default:
+				printf("ERROR: Unknown option -%c\n", opt);
+				exit(-1);
+		}
+
+	if (!passwd)
+	{
+		fprintf(stderr, "ERROR: You must specify an operator password\n");
+		exit(-1);
+	}
+
+        int serverSocket;
+	int clientSocket;
+	struct sockaddr_in serverAddr, clientAddr;
+	int yes = 1; // I'm not sure what this variable is
+	socklen_t sinSize = sizeof(struct sockaddr_in);
+
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(atoi(port));
+        serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+	serverSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+	bind(serverSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+	listen(serverSocket, 5);
+
+	while(1)
+	{
+		clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddr, &sinSize);
+		send(clientSocket, "LOL IT Worked", strlen("LOL IT Worked"), 0);
+		int msgLen = 512;
+		char inputBuf[msgLen];
+		memset(inputBuf, 0, msgLen); //set input to NULL
+
+		int nbytes;
+		while( (nbytes = recv(clientSocket, inputBuf, msgLen, 0)) )
+		{
+			if ((inputBuf[msgLen-1]) && (inputBuf[msgLen-1]!='\n'))
+			{
+				inputBuf[msgLen-2]='\r';
+				inputBuf[msgLen-1]='\n';//should a message be saved if it is cut off?
+			}
+			printf("I got your message! It is: %s", inputBuf); //remove this test later
+			char **argList;
+			int maxArgs = 15;
+			argList = (char **) malloc((1+maxArgs)*sizeof(char **));
+			int argNum = parser(inputBuf, nbytes, argList);
+			printf("Number of args:%d\n", argNum); //remove later 
+
+			free(argList);
+
+			memset(inputBuf, 0, msgLen); //clear buffer
+		}
+	}
+
+	close(serverSocket);
+
+
+	return 0;
+}
+
