4612517d4d28661f36e102febf827872edff7538
Laura Macaddino <l.macaddino@gmail.com>
2013-04-07 02:53:44 -0500

    Now adding two error response messages: ALREADYREGISTERED and INVALID COMMAND.

---
+++ src/command.h
@@ -0,0 +1,17 @@
+/*
+ *
+ *  CMSC 23300 / 33300 - Networks and Distributed Systems
+ *
+ *  Command codes
+ *
+ */
+
+#ifndef COMMAND_H_
+#define COMMAND_H_
+
+#define COMMANDNUM 2
+
+#define NICK 	0
+#define USER 	1
+
+#endif /* COMMAND_H_ */
